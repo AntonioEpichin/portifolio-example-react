65aceca74ed762504be6550387df5eb6809198f9
Replit user <>
2024-02-02 19:45:53 +0000

    job's done!

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,6 +32,29 @@ const socials = [
   },
 ];
 const Header = () => {
+  const headerRef = useRef(); // useRef to refer to the header DOM element
+  const lastScrollY = useRef(0); // useRef to keep track of the last scroll position
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY < lastScrollY.current) {
+        // Scrolling up
+        headerRef.current.style.transform = "translateY(0)";
+      } else if (currentScrollY > lastScrollY.current) {
+        // Scrolling down
+        headerRef.current.style.transform = "translateY(-200px)";
+      }
+      lastScrollY.current = currentScrollY; // Update the last scroll position
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const handleClick = (anchor) => () => {
     const id = `${anchor}-section`;
     const element = document.getElementById(id);
@@ -45,15 +68,16 @@ const Header = () => {
 
   return (
     <Box
+      ref={headerRef} // Attach the ref to the Box component
       position="fixed"
       top={0}
       left={0}
       right={0}
-      translateY={0}
       transitionProperty="transform"
       transitionDuration=".3s"
       transitionTimingFunction="ease-in-out"
       backgroundColor="#18181b"
+      zIndex="10"
     >
       <Box color="white" maxWidth="1280px" margin="0 auto">
         <HStack
@@ -65,7 +89,12 @@ const Header = () => {
           <nav>
             <HStack spacing={4}>
               {socials.map((social, index) => (
-                <a key={index} href={social.url} target="_blank" rel="noopener noreferrer">
+                <a
+                  key={index}
+                  href={social.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <FontAwesomeIcon icon={social.icon} size="2x" />
                 </a>
               ))}
@@ -73,10 +102,18 @@ const Header = () => {
           </nav>
           <nav>
             <HStack spacing={8}>
-              <a href="/#projects-section" onClick={handleClick('projects')} style={{ cursor: 'pointer' }}>
+              <a
+                href="/#projects-section"
+                onClick={handleClick("projects")}
+                style={{ cursor: "pointer" }}
+              >
                 Projects
               </a>
-              <a href="/#contactme-section" onClick={handleClick('contactme')} style={{ cursor: 'pointer' }}>
+              <a
+                href="/#contactme-section"
+                onClick={handleClick("contactme")}
+                style={{ cursor: "pointer" }}
+              >
                 Contact Me
               </a>
             </HStack>
